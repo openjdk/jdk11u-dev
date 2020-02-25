2c52a81a1c438b956deaf340b3359e5694d0cd65
Severin Gehwolf <sgehwolf@openjdk.org>
2020-02-25 12:17:26 +0100

    8239785: Cgroups: Incorrect detection logic on old systems in hotspot
    Return NULL subsystem if no cgroup controllers are mounted.
    
    Reviewed-by: bobv, mbaesken

--- test/lib/sun/hotspot/WhiteBox.java
+++ test/lib/sun/hotspot/WhiteBox.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2019, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2020, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -538,6 +538,9 @@ public class WhiteBox {
 
   // Container testing
   public native boolean isContainerized();
+  public native int validateCgroup(String procCgroups,
+                                   String procSelfCgroup,
+                                   String procSelfMountinfo);
   public native void printOsInfo();
 
   // Decoder
