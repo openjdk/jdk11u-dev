2e35ef2d945448657604169dc81fcd7fce5fce96
tstuefe <thomas.stuefe@gmail.com>
2021-11-29 09:20:03 +0100

    Backport 735757f1e0ec9d05eb4c6f4d680b34de96f83700

--- test/hotspot/jtreg/gc/stress/TestReclaimStringsLeaksMemory.java
+++ test/hotspot/jtreg/gc/stress/TestReclaimStringsLeaksMemory.java
@@ -48,9 +48,9 @@ import jdk.test.lib.process.ProcessTools;
 
 public class TestReclaimStringsLeaksMemory {
 
-    // The amount of memory in kB reserved in the "Symbol" category that indicates a memory leak for
+    // The amount of memory in B reserved in the "Symbol" category that indicates a memory leak for
     // this test.
-    public static final int ReservedThreshold = 70000;
+    public static final int ReservedThreshold = 70000000;
 
     public static void main(String[] args) throws Exception {
         ArrayList<String> baseargs = new ArrayList(Arrays.asList( "-Xms256M",
@@ -78,7 +78,7 @@ public class TestReclaimStringsLeaksMemory {
         }
 
         int reserved = Integer.parseInt(m.group(1));
-        Asserts.assertLT(reserved, ReservedThreshold, "Reserved memory size is " + reserved + "KB which is greater than or equal to " + ReservedThreshold + "KB indicating a memory leak");
+        Asserts.assertLT(reserved, ReservedThreshold, "Reserved memory size is " + reserved + "B which is greater than or equal to " + ReservedThreshold + "B indicating a memory leak");
 
         output.shouldHaveExitValue(0);
     }
