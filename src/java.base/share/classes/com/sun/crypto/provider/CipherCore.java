e893476927ff77aebc0e8c7b21ae1a8fa5dacac7
Scott Gibbons <scott.gibbons@intel.com>
2023-02-27 09:54:41 -0800

    Backport 409382ba4b43bf48ed0086020dd20641effd35b6

--- src/java.base/share/classes/com/sun/crypto/provider/CipherCore.java
+++ src/java.base/share/classes/com/sun/crypto/provider/CipherCore.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2002, 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2002, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -932,9 +932,13 @@ final class CipherCore {
             if (outputCapacity < estOutSize) {
                 cipher.save();
             }
-            // create temporary output buffer so that only "real"
-            // data bytes are passed to user's output buffer.
-            outWithPadding = new byte[estOutSize];
+            if (outputCapacity < estOutSize || padding != null) {
+                // create temporary output buffer if the estimated size is larger
+                // than the user-provided buffer or a padding needs to be removed
+                // before copying the unpadded result to the output buffer
+                outWithPadding = new byte[estOutSize];
+                offset = 0;
+            }
         }
         byte[] outBuffer = decrypting ? outWithPadding : output;
 
