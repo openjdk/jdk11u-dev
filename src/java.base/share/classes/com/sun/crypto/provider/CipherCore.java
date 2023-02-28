6d375967fab8418bb6e8a6e3d1fc6693828cc0eb
Scott Gibbons <scott.gibbons@intel.com>
2023-02-28 11:43:22 -0800

    Fixed bad merge

--- src/java.base/share/classes/com/sun/crypto/provider/CipherCore.java
+++ src/java.base/share/classes/com/sun/crypto/provider/CipherCore.java
@@ -940,7 +940,7 @@ final class CipherCore {
                 offset = 0;
             }
         }
-        byte[] outBuffer = (outWithPadding != null) ? outWithPadding : output;
+        byte[] outBuffer = (decrypting && (outWithPadding != null)) ? outWithPadding : output;
 
         int outLen = fillOutputBuffer(finalBuf, finalOffset, outBuffer,
                 offset, finalBufLen, input);
