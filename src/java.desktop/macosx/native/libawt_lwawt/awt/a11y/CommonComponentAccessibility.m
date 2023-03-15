372dcd97e49d89c310e87fbbf0eba76ec54846f7
Autumn808 <AutumnCapasso@gmail.com>
2023-03-15 13:57:55 -0700

    Backport 45e05970bcee842f98eeeef054f574ccf4b43fad

--- src/java.desktop/macosx/native/libawt_lwawt/awt/a11y/CommonComponentAccessibility.m
+++ src/java.desktop/macosx/native/libawt_lwawt/awt/a11y/CommonComponentAccessibility.m
@@ -48,7 +48,7 @@ static jobject sAccessibilityClass = NULL;
     /*
      * Here we should keep all the mapping between the accessibility roles and implementing classes
      */
-    rolesMap = [[NSMutableDictionary alloc] initWithCapacity:35];
+    rolesMap = [[NSMutableDictionary alloc] initWithCapacity:36];
 
 
     [rolesMap setObject:@"ButtonAccessibility" forKey:@"pushbutton"];
@@ -68,6 +68,7 @@ static jobject sAccessibilityClass = NULL;
     [rolesMap setObject:@"GroupAccessibility" forKey:@"internalframe"];
     [rolesMap setObject:@"GroupAccessibility" forKey:@"swingcomponent"];
     [rolesMap setObject:@"ToolbarAccessibility" forKey:@"toolbar"];
+    [rolesMap setObject:@"SplitpaneAccessibility" forKey:@"splitpane"];
 
 
     /*
