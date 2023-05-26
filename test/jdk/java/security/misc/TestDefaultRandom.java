5cf3b10ecddd3ac42829457fc30d66d4ebb6777e
Lutz Schmidt <Lutz.Schmidt@sap.com>
2023-05-26 14:48:36 +0200

    JDK-8260274: avoid use of switch expressions

--- test/jdk/java/security/misc/TestDefaultRandom.java
+++ test/jdk/java/security/misc/TestDefaultRandom.java
@@ -62,16 +62,23 @@ public class TestDefaultRandom {
         }
 
         switch (argv[0]) {
-            case "APG" ->
+            case "APG":
                 check(AlgorithmParameterGenerator.getInstance("DSA"), argv[1]);
-            case "KPG" ->
+                break;
+            case "KPG":
                 check(KeyPairGenerator.getInstance("RSA"), argv[1]);
-            case "CIP" ->
+                break;
+            case "CIP":
                 check(Cipher.getInstance("AES/CBC/NoPadding"), argv[1]);
-            case "KA" -> check(KeyAgreement.getInstance("DH"), argv[1]);
-            case "KG" -> check(KeyGenerator.getInstance("AES"), argv[1]);
-            default -> throw new RuntimeException
-                    ("Error: unsupported test type");
+                break;
+            case "KA":
+                check(KeyAgreement.getInstance("DH"), argv[1]);
+                break;
+            case "KG":
+                check(KeyGenerator.getInstance("AES"), argv[1]);
+                break;
+            default:
+                throw new RuntimeException("Error: unsupported test type");
         }
     }
 
@@ -92,47 +99,70 @@ public class TestDefaultRandom {
                 AlgorithmParameterGenerator apg =
                         (AlgorithmParameterGenerator) obj;
                 switch (testNum) {
-                    case "1" -> apg.init(123);
-                    case "2" -> apg.init((AlgorithmParameterSpec) null);
-                    default -> throw new RuntimeException
-                            ("Error: invalid test#");
+                    case "1":
+                        apg.init(123);
+                        break;
+                    case "2":
+                        apg.init((AlgorithmParameterSpec) null);
+                        break;
+                    default:
+                        throw new RuntimeException("Error: invalid test#");
                 }
             } else if (obj instanceof KeyPairGenerator) {
                 KeyPairGenerator kpg = (KeyPairGenerator) obj;
                 switch (testNum) {
-                    case "1" -> kpg.initialize(123);
-                    case "2" -> kpg.initialize((AlgorithmParameterSpec) null);
-                    default -> throw new RuntimeException
-                            ("Error: invalid test#");
+                    case "1":
+                        kpg.initialize(123);
+                        break;
+                    case "2":
+                        kpg.initialize((AlgorithmParameterSpec) null);
+                        break;
+                    default:
+                        throw new RuntimeException("Error: invalid test#");
                 }
             } else if (obj instanceof Cipher) {
                 Cipher c = (Cipher) obj;
                 switch (testNum) {
-                    case "1" -> c.init(Cipher.ENCRYPT_MODE, (Key) null);
-                    case "2" -> c.init(Cipher.ENCRYPT_MODE, (Key) null,
+                    case "1":
+                        c.init(Cipher.ENCRYPT_MODE, (Key) null);
+                        break;
+                    case "2":
+                        c.init(Cipher.ENCRYPT_MODE, (Key) null,
                         (AlgorithmParameterSpec) null);
-                    case "3" -> c.init(Cipher.ENCRYPT_MODE, (Key) null,
+                        break;
+                    case "3":
+                        c.init(Cipher.ENCRYPT_MODE, (Key) null,
                         (AlgorithmParameters) null);
-                    case "4" -> c.init(Cipher.ENCRYPT_MODE, (Certificate)null);
-                    default -> throw new RuntimeException
-                            ("Error: invalid test#");
+                        break;
+                    case "4":
+                        c.init(Cipher.ENCRYPT_MODE, (Certificate)null);
+                        break;
+                    default:
+                        throw new RuntimeException("Error: invalid test#");
                 }
             } else if (obj instanceof KeyAgreement) {
                 KeyAgreement ka = (KeyAgreement) obj;
                 switch (testNum) {
-                    case "1" -> ka.init((Key) null);
-                    case "2" -> ka.init((Key) null, (AlgorithmParameterSpec)
-                            null);
-                    default -> throw new RuntimeException
-                            ("Error: invalid test#");
+                    case "1":
+                        ka.init((Key) null);
+                        break;
+                    case "2":
+                        ka.init((Key) null, (AlgorithmParameterSpec) null);
+                        break;
+                    default:
+                        throw new RuntimeException("Error: invalid test#");
                 }
             } else if (obj instanceof KeyGenerator) {
                 KeyGenerator kg = (KeyGenerator) obj;
                 switch (testNum) {
-                    case "1" -> kg.init(123);
-                    case "2" -> kg.init((AlgorithmParameterSpec) null);
-                    default -> throw new RuntimeException
-                            ("Error: invalid test#");
+                    case "1":
+                        kg.init(123);
+                        break;
+                    case "2":
+                        kg.init((AlgorithmParameterSpec) null);
+                        break;
+                    default:
+                        throw new RuntimeException("Error: invalid test#");
                 }
             } else {
                 throw new RuntimeException("Error: Unsupported type");
