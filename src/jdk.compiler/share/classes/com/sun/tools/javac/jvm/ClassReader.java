797796792b7bda7e69927370946bd10e444e85ed
Goetz Lindenmaier <Goetz.Lindenmaier@sap.com>
2024-01-10 10:07:54 +0100

    Revert "8320001: javac crashes while adding type annotations to the return type of a constructor"
    This reverts commit 02df756f4f9288fef7abe377e089a82f6908352d.

--- src/jdk.compiler/share/classes/com/sun/tools/javac/jvm/ClassReader.java
+++ src/jdk.compiler/share/classes/com/sun/tools/javac/jvm/ClassReader.java
@@ -2458,12 +2458,7 @@ public class ClassReader {
                 thrown.add(addTypeAnnotations(thrownType, thrownType(i++)));
             }
             mt.thrown = thrown.toList();
-            /* possible information loss if the type of the method is void then we can't add type
-             * annotations to it
-             */
-            if (!mt.restype.hasTag(TypeTag.VOID)) {
-                mt.restype = addTypeAnnotations(mt.restype, TargetType.METHOD_RETURN);
-            }
+            mt.restype = addTypeAnnotations(mt.restype, TargetType.METHOD_RETURN);
             if (mt.recvtype != null) {
                 mt.recvtype = addTypeAnnotations(mt.recvtype, TargetType.METHOD_RECEIVER);
             }
