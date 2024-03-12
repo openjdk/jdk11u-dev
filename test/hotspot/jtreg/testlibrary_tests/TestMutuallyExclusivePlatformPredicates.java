63b585b88a44c61829ef5d4be92720e90a6d7da6
i556354 <andrew.lu02@sap.com>
2024-03-12 17:15:51 +0800

    Backport 1f9b03e597d87679964e8772c6bea538c74feb2c

--- test/hotspot/jtreg/testlibrary_tests/TestMutuallyExclusivePlatformPredicates.java
+++ test/hotspot/jtreg/testlibrary_tests/TestMutuallyExclusivePlatformPredicates.java
@@ -52,7 +52,7 @@ public class TestMutuallyExclusivePlatformPredicates {
         MODE("isInt", "isMixed", "isComp"),
         IGNORED("isEmulatedClient", "isDebugBuild", "isFastDebugBuild", "isSlowDebugBuild",
                 "hasSA", "isRoot", "isTieredSupported", "areCustomLoadersSupportedForCDS",
-                "isSignedOSX");
+                "isSignedOSX", "isOracleLinux7");
 
         public final List<String> methodNames;
 
