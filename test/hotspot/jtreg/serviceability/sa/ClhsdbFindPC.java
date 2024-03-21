00a86de70035fbe371866d86f38a25efccbf459a
i556354 <andrew.lu02@sap.com>
2024-03-21 13:26:31 +0800

    Backport 16e0ad0ad088af3ba1c9903ed8df60799a1ba651

--- test/hotspot/jtreg/serviceability/sa/ClhsdbFindPC.java
+++ test/hotspot/jtreg/serviceability/sa/ClhsdbFindPC.java
@@ -27,63 +27,26 @@ import java.util.Map;
 import java.util.ArrayList;
 
 import jdk.test.lib.apps.LingeredApp;
-import jdk.test.lib.util.CoreUtils;
 import jtreg.SkippedException;
 
 /**
  * @test
  * @bug 8193124
- * @summary Test the clhsdb 'findpc' command with Xcomp on live process
- * @requires vm.hasSA
- * @requires vm.compiler1.enabled
- * @requires vm.opt.DeoptimizeALot != true
- * @library /test/lib
- * @run main/othervm/timeout=480 ClhsdbFindPC true false
- */
-
-/**
- * @test
- * @bug 8193124
- * @summary Test the clhsdb 'findpc' command with Xcomp on core file
- * @requires vm.compMode != "Xcomp"
+ * @summary Test the clhsdb 'findpc' command
  * @requires vm.hasSA
  * @requires vm.compiler1.enabled
  * @library /test/lib
- * @run main/othervm/timeout=480 ClhsdbFindPC true true
- */
-
-/**
- * @test
- * @bug 8193124
- * @summary Test the clhsdb 'findpc' command w/o Xcomp on live process
- * @requires vm.hasSA
- * @requires vm.compiler1.enabled
- * @requires vm.opt.DeoptimizeALot != true
- * @library /test/lib
- * @run main/othervm/timeout=480 ClhsdbFindPC false false
- */
-
-/**
- * @test
- * @bug 8193124
- * @summary Test the clhsdb 'findpc' command w/o Xcomp on core file
- * @requires vm.compMode != "Xcomp"
- * @requires vm.hasSA
- * @requires vm.compiler1.enabled
- * @library /test/lib
- * @run main/othervm/timeout=480 ClhsdbFindPC false true
+ * @run main/othervm/timeout=480 ClhsdbFindPC
  */
 
 public class ClhsdbFindPC {
 
-    private static void testFindPC(boolean withXcomp, boolean withCore) throws Exception {
+    private static void testFindPC(boolean withXcomp) throws Exception {
         LingeredApp theApp = null;
-        String coreFileName = null;
         try {
             ClhsdbLauncher test = new ClhsdbLauncher();
 
             theApp = new LingeredAppWithTrivialMain();
-            theApp.setForceCrash(withCore);
             if (withXcomp) {
                 LingeredApp.startApp(List.of("-Xcomp"), theApp);
             } else {
@@ -97,41 +60,27 @@ public class ClhsdbFindPC {
             }
             System.out.println("with pid " + theApp.getPid());
 
-            // Get the core file name if we are debugging a core instead of live process
-            if (withCore) {
-                coreFileName = CoreUtils.getCoreFileLocation(theApp.getOutput().getStdout());
-            }
-
-            // Run 'jstack -v' command to get the findpc address
+            // Run 'jstack -v' command to get the pc
             List<String> cmds = List.of("jstack -v");
-            String output;
-            if (withCore) {
-                output = test.runOnCore(coreFileName, cmds, null, null);
-            } else {
-                output = test.run(theApp.getPid(), cmds, null, null);
-            }
+            String output = test.run(theApp.getPid(), cmds, null, null);
 
-            // Extract pc address from the following line:
-            //   - LingeredAppWithTrivialMain.main(java.lang.String[]) @bci=1, line=33, pc=0x00007ff18ff519f0, ...
-            String pcAddress = null;
+            // Test the 'findpc' command passing in the pc obtained from
+            // the 'jstack -v' command
+            cmds = new ArrayList<String>();
+
+            String cmdStr = null;
             String[] parts = output.split("LingeredAppWithTrivialMain.main");
             String[] tokens = parts[1].split(" ");
             for (String token : tokens) {
                 if (token.contains("pc")) {
-                    String[] addresses = token.split("=");
-                    // addresses[1] represents the address of the Method
-                    pcAddress = addresses[1].replace(",","");
+                    String[] address = token.split("=");
+                    // address[1] represents the address of the Method
+                    cmdStr = "findpc " + address[1].replace(",","");
+                    cmds.add(cmdStr);
                     break;
                 }
             }
-            if (pcAddress == null) {
-                throw new RuntimeException("Cannot find LingeredAppWithTrivialMain.main pc in output");
-            }
 
-            // Test the 'findpc' command passing in the pc obtained from above
-            cmds = new ArrayList<String>();
-            String cmdStr = "findpc " + pcAddress;
-            cmds.add(cmdStr);
             Map<String, List<String>> expStrMap = new HashMap<>();
             if (withXcomp) {
                 expStrMap.put(cmdStr, List.of(
@@ -144,27 +93,20 @@ public class ClhsdbFindPC {
                             "In interpreter codelet"));
             }
 
-            if (withCore) {
-                test.runOnCore(coreFileName, cmds, expStrMap, null);
-            } else {
-                test.run(theApp.getPid(), cmds, expStrMap, null);
-            }
+            test.run(theApp.getPid(), cmds, expStrMap, null);
         } catch (SkippedException se) {
             throw se;
         } catch (Exception ex) {
             throw new RuntimeException("Test ERROR " + ex, ex);
         } finally {
-            if (!withCore) {
-                LingeredApp.stopApp(theApp);
-            }
+            LingeredApp.stopApp(theApp);
         }
     }
 
     public static void main(String[] args) throws Exception {
-        boolean withXcomp = Boolean.parseBoolean(args[0]);
-        boolean withCore = Boolean.parseBoolean(args[1]);
         System.out.println("Starting the ClhsdbFindPC test");
-        testFindPC(withXcomp, withCore);
+        testFindPC(true);
+        testFindPC(false);
         System.out.println("Test PASSED");
     }
 }
