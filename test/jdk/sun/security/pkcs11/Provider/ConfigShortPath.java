afeaa6fa966b29db87cf6d6033a9663ab568e55e
KIRIYAMA Takuya <kiriyama.takuya@fujitsu.com>
2026-07-09 21:12:08 +0900

    8365861: test/jdk/sun/security/pkcs11/Provider/ tests skipped without SkippedException

--- test/jdk/sun/security/pkcs11/Provider/ConfigShortPath.java
+++ test/jdk/sun/security/pkcs11/Provider/ConfigShortPath.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2010, 2015, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2010, 2025, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -25,11 +25,17 @@
  * @bug 6581254 6986789 7196009 8062170
  * @summary Allow '~', '+', and quoted paths in config file
  * @author Valerie Peng
+ * @library /test/lib
  */
 
-import java.security.*;
-import java.io.*;
-import java.lang.reflect.*;
+import jtreg.SkippedException;
+
+import java.io.File;
+import java.io.IOException;
+import java.security.InvalidParameterException;
+import java.security.Provider;
+import java.security.ProviderException;
+import java.security.Security;
 
 public class ConfigShortPath {
 
@@ -46,8 +52,7 @@ public class ConfigShortPath {
             // re-try w/ SunPKCS11-Solaris
             p = Security.getProvider("SunPKCS11-Solaris");
             if (p == null) {
-                System.out.println("Skipping test - no PKCS11 provider available");
-                return;
+				throw new SkippedException("Skipping test - no PKCS11 provider available");
             }
         }
 
@@ -69,7 +74,7 @@ public class ConfigShortPath {
                 if (cause.getClass().getName().equals
                         ("sun.security.pkcs11.ConfigurationException")) {
                     // Error occurred during parsing
-                    if (cause.getMessage().indexOf("Unexpected") != -1) {
+                    if (cause.getMessage().contains("Unexpected")) {
                         throw (ProviderException) cause;
                     }
                 }
